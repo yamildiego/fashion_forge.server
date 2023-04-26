d1b097ffc435f040cffe44cb74a977a1f2c9c634
Yamil Diego <yamildiego@gmail.com>
2023-04-26 11:31:16 +1000

    fix: misspelling var

--- config/mailConfig.ts
+++ config/mailConfig.ts
@@ -4,7 +4,7 @@ const mailConfig = {
   host: Env.get('SMTP_HOST'),
   port: Env.get('SMTP_PORT'),
   auth: {
-    user: Env.get('SMTP_PORT'),
+    user: Env.get('SMTP_USERNAME'),
     pass: Env.get('SMTP_PASSWORD'),
   },
 }
